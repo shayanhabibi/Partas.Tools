ea0bedd9b84a02f8100b40e9656d61a53f44589e
cabboose <shayanftw@gmail.com>
2025-07-31 11:30:58 +0800

    feat: Init SepochSemver

---
+++ Partas.Tools.SepochSemver/Program.fs
@@ -0,0 +1,75 @@
+﻿module Partas.Tools.SepochSemver
+
+open Fake.Core
+open Semver
+open XParsec
+open XParsec.CharParsers
+open XParsec.Parsers
+
+[<RequireQualifiedAccess; Struct>]
+type Sepoch =
+    | Scope of scope: string
+    | EpochScope of epoch: string * scope: string
+    | Epoch of epoch: string
+    | None
+
+[<Struct>]
+type SepochSemver = {
+    SemVer: SemVersion
+    Sepoch: Sepoch
+}
+
+/// <summary>
+/// Parses a Epoch & Scope if it is present and consumes the input.
+/// </summary>
+/// <remarks>
+/// <para>Epoch or Scope are a SemVer prefix bounded by <c>&lt;></c> angle brackets.</para>
+/// <para>Scope is itself bounded within by parenthesis.</para>
+/// </remarks>
+let private parseSepoch input =
+    // XParsec reader
+    let reader = Reader.ofString input ()
+    // Valid chars for a Sepoch
+    let sepochChars = noneOf "1234567890<>+-. \n\r\n#"
+    // The opening delimiter of a Sepoch
+    let openBound = skipChar '<'
+    // The closing delimiters of an EPOCH
+    // Does not consume the closing delimiter for the epoch
+    let terminator =
+        lookAhead (skipChar '>' <|> skipChar '(')
+    // Consumes an epoch
+    let consumeEpoch =
+        manyCharsTill sepochChars terminator
+    // Consumes a scope
+    let consumeScope =
+        let terminator = skipChar ')'
+        let consume = skipChar '(' >>. manyCharsTill sepochChars terminator
+        (opt consume) .>> skipChar '>'
+    // Combines the results of consumeEpoch and consumeScope
+    let combinator = fun (epoch: string * unit) (scope: (string * unit) voption) ->
+        fst epoch, scope |> ValueOption.map fst
+    // The Sepoch parse result (optional)
+    let sepoch = reader |> opt (openBound >>. pipe2 consumeEpoch consumeScope combinator)
+    // The rest of the input to be fed into the SemVer parser
+    let remainder = reader |> manyChars anyChar
+    // Matching the results of the sepoch parse result and the rest of the input
+    match sepoch,remainder with
+    | Ok { Parsed = ValueSome sepoch }, Ok { Parsed = remainder } ->
+        remainder |> String.trim,
+        match sepoch with
+        | epoch, ValueSome scope when epoch |> String.isNullOrWhiteSpace -> Sepoch.Scope scope
+        | epoch, ValueNone when epoch |> String.isNullOrWhiteSpace -> Sepoch.None
+        | epoch, ValueSome scope -> Sepoch.EpochScope(epoch,scope)
+        | epoch, ValueNone -> Sepoch.Epoch(epoch)
+    | _, Ok { Parsed = remainder } ->
+        remainder |> String.trim, Sepoch.None
+    | results -> failwithf $"%A{results}"
+
+let inline private parseSemver input = SemVersion.Parse input
+
+let parseSepochSemver input =
+    let input,sepoch = parseSepoch input
+    {
+        Sepoch = sepoch
+        SemVer = parseSemver input
+    }
---
+++ Partas.Tools.SepochSemver/README.md
@@ -0,0 +1,78 @@
+﻿# SepochSemver
+
+My own flavor of Semver with a proposed marketing value `Epoch`, with an added optional `Scope` for
+disambiguating tags for SemVer packages in a monorepo.
+
+## Spec
+
+### Semver
+
+Semver spec follows the SemVer 2.0 spec and uses the [NuGet `Semver` package].
+
+### Sepoch - Scoped Epoch
+
+A Sepoch is an optional identifier preceding a Semver delimited by `<` angle brackets `>`.
+
+It SHOULD have a single whitespace delineating it from the Semver
+
+```
+[<SEPOCH>] SEMVER
+```
+
+A Sepoch cannot contain spaces, **numbers**, and other special characters such as `.` `+` `-` which are used elsewhere in Semver.
+
+> [!NOTE]
+> The exclusion of numbers simplifies visual identification and disentanglement of the Sepoch from the Semver.
+> 
+> It also simplifies tooling by providing an entire region that can be skipped - identifying the semver by the
+> presence of the first numeric character.
+
+`(` Parenthesis `)` may only be used in Sepoch to delineate the Scope.
+
+A Sepoch is made up of two optional identifiers in the following order:
+1. Epoch
+   - Alphabetical identifier
+   - SHOULD be in uppercase
+2. Scope
+   - Alphabetical identifier bound by `(` parenthesis `)`
+   - SHOULD be capitalized
+   - SHOULD NOT be in uppercase
+
+## Usage
+
+```fsharp
+open Partas.Tools.SepochSemver
+
+let sepochSemver: SepochSemver = parseSepochSemver "<WILDLANDS(Core)> 1.4.0"
+```
+
+```fsharp
+[<RequireQualifiedAccess; Struct>]
+type Sepoch =
+    | Scope of scope: string
+    | EpochScope of epoch: string * scope: string
+    | Epoch of epoch: string
+    | None
+
+[<Struct>]
+type SepochSemver = {
+    SemVer: SemVersion
+    Sepoch: Sepoch
+}
+```
+
+## Dependencies
+
+- [XParsec] for Sepoch parsing
+- [Semver] for Semver parsing
+- [Fake.Core.Strings] for string helpers
+
+[XParsec]:[https://github.com/roboz0r/XParsec]
+[Semver]:[https://semver-nuget.org/]
+[Fake.Core.Strings]:[https://fake.build/]
+
+---
+
+MIT License
+
+Copyright (c) Shayan Habibi 2025
